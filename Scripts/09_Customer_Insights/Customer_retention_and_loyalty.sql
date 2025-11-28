d856e6a3730171eb17093136a666661803b18792
Hafeezuddin <hafeezskhbablu@gmail.com>
2025-11-27 21:16:08 -0500

    Metric #1 calculated and added

--- Scripts/09_Customer_Insights/Customer_retention_and_loyalty.sql
+++ Scripts/09_Customer_Insights/Customer_retention_and_loyalty.sql
@@ -1,40 +1,64 @@
 /* Business Problem: Customer Retention & Loyalty Program Optimization
-Business Context:The DVD rental company is facing increased competition from streaming services. They want to launch a targeted loyalty program to retain their most valuable customers and win back at-risk customers.
+Business Context:The DVD rental company is facing increased competition from streaming services. 
+They want to launch a targeted loyalty program to retain their most valuable customers and win back at-risk customers.
 
 Business Questions:
 "Who are our most valuable customers and what are their rental patterns?"
 "Which customers are at risk of churning based on declining activity?"
 "What film categories and actors drive the most customer loyalty?"
 "How should we segment customers for targeted marketing campaigns?"
-"What rewards would be most effective for each customer segment?"
-Available Data (Sakila Schema):
 
+Available Data (Sakila Schema):
 Customer Demographics: customer table with join dates
 Rental History: rental table with timestamps
 Payment Data: payment table for customer lifetime value
 Film Preferences: film, film_category, category tables
 Actor Preferences: film_actor, actor tables
 Store Locations: store, address tables for geographic patterns
-Analytical Framework:
 
-Metric 1: Customer Value Scoring
-Recency, Frequency, Monetary (RFM) analysis
-Customer Lifetime Value calculation
+Analytical Framework:
+Metric 1: Customer Value Scoring:
+Recency, Frequency, Customer Lifetime Value calculation, 
+Monetary (RFM) analysis - Based on total_money_spent, total_rentals, days_since_last_rental
 Rental frequency trends over time
 
-Metric 2: Churn Risk Assessment
+Metric 2: Churn Risk Assessment:
 Months since last rental
 Rental frequency decline rate
 Payment pattern changes
 
-Metric 3: Content Preference Analysis
+Metric 3: Content Preference Analysis:
 Favorite categories per customer segment
 Actor popularity across value tiers
 Seasonal rental patterns
-Required Deliverables:
 
+Required Deliverables:
 Customer segmentation with clear value tiers
 Churn risk scoring with at-risk customer list
 Personalized film recommendations for each segment
 Loyalty program structure with tier-specific benefits
 Expected ROI for retention campaigns */
+
+--CTE to calculate metric #1
+customer_value_scoring AS (
+    SELECT c.customer_id, 
+        MAX(r.rental_date::date) AS latest_rental_date,
+        --Assuming current_Date to be 12-31-2005 (Cut off date)
+        --CURRENT_DATE - MAX(r.rental_date::date) AS days_since_last_rental to be used if the data us current.
+        '01-01-2006' - MAX(r.rental_date::date) AS days_since_last_rental,
+        NTILE(4) OVER (ORDER BY '01-01-2006' - MAX(r.rental_date::date) DESC) AS recency_quartile,
+        COUNT(DISTINCT r.rental_id) AS life_time_rentals,
+        NTILE(4) OVER (ORDER BY COUNT(DISTINCT r.rental_id)) AS total_rentals_quartile,
+        SUM(p.amount) AS total_spent,
+        NTILE(4) OVER (ORDER BY SUM(p.amount)) AS spent_quartile,
+        COUNT(DISTINCT r.rental_id)/COUNT (DISTINCT EXTRACT(MONTH FROM r.rental_date)) AS average_active_month_rentals
+    FROM customer c
+    INNER JOIN rental r ON c.customer_id = r.customer_id
+    INNER JOIN payment p ON r.rental_id = p.rental_id
+    WHERE r.return_date IS NOT NULL and (r.rental_date BETWEEN '01-01-2005' AND '12-31-2005')
+    GROUP BY 1
+) 
+
+EXTRACT()
+DATE_TRUNC()
+TO_CHAR()
