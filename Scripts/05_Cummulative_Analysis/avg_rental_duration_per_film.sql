d67bf4fc3d34b30973771706300e1570b67dadae
Hafeezuddin <hafeezskhbablu@gmail.com>
2025-12-23 20:21:01 -0500

    Added 2005 constraint

--- Scripts/05_Cummulative_Analysis/avg_rental_duration_per_film.sql
+++ Scripts/05_Cummulative_Analysis/avg_rental_duration_per_film.sql
@@ -1,14 +1,11 @@
 /* Average rental duration per film */
-SELECT f.film_id,
-  f.title,
-  EXTRACT(
-    DAY
-    FROM AVG(r.return_date - r.rental_date)
-  ) AS avg_rental_days
+SELECT f.film_id, f.title,
+  EXTRACT(DAY FROM AVG(r.return_date - r.rental_date)) AS avg_rental_days
 FROM film f
   JOIN inventory i ON f.film_id = i.film_id
   JOIN rental r ON i.inventory_id = r.inventory_id
-WHERE r.return_date IS NOT NULL
+  JOIN payment p ON r.rental_id = p.rental_id
+WHERE r.return_date IS NOT NULL AND (r.rental_date >= '2005-01-01' AND r.rental_date <= '2005-12-31')
 GROUP BY f.film_id,
   f.title
 ORDER BY avg_rental_days DESC;
