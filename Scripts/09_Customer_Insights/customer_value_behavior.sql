c445d45db56670ce0cf2f29784dbc2e4a2cef3ca
Hafeezuddin <hafeezskhbablu@gmail.com>
2026-01-03 22:45:13 -0500

    Add: fav cat, fav store, who served them most, aggregated into main query

--- Scripts/09_Customer_Insights/customer_value_behavior.sql
+++ Scripts/09_Customer_Insights/customer_value_behavior.sql
@@ -7,7 +7,6 @@ Their lifetime value (total payments)
 Rental frequency (rentals per month)
 Last rental date
 Days since last rental
-
 Favorite category (most rented)
 Store they primarily use
 Staff member who usually helps them
@@ -23,6 +22,7 @@ Flag customers who haven't rented in > 150 days as "at-risk" **/
 WITH customer_insights AS (SELECT c.customer_id,
                                   EXTRACT(MONTH FROM age(MAX(r.rental_date), MIN(r.rental_date))) + 1             AS active_months,
                                   SUM(p.amount)                                                                   AS life_time_payments,
+                                  percent_rank() OVER (ORDER BY SUM(p.amount) DESC)                               AS life_time_value_percent_rank,
                                   to_char(DATE_TRUNC('day', MAX(r.rental_date)), 'YYYY-MM-DD')                    AS last_rental_date,
                                   EXTRACT(DAYS FROM '01-01-2006' - MAX(r.rental_date))                            AS days_since_last_rental,
                                   COUNT(distinct r.rental_id)                                                     AS total_rentals, --Paid rentals
@@ -34,7 +34,13 @@ WITH customer_insights AS (SELECT c.customer_id,
                                           THEN 'active'
                                       ELSE
                                           'Inactive'
-                                      END                                                                         AS active_status
+                                      END                                                                         AS active_status,
+                                  CASE
+                                      WHEN '2006-01-01' - MAX(r.rental_date::date) < 150
+                                          THEN 'Not at Risk'
+                                      ELSE
+                                          'At Risk'
+                                      END AS                                                                      risk_status
                            FROM customer c
                                     INNER JOIN rental r ON c.customer_id = r.customer_id
                                     INNER JOIN payment p ON r.rental_id = p.rental_id
@@ -47,14 +53,35 @@ fav_cat AS (SELECT t1.customer_id, t1.name
             FROM (SELECT ci.customer_id,
                          ct.name,
                          COUNT(DISTINCT r.rental_id),
-                         RANK() OVER (PARTITION BY ci.customer_id ORDER BY count(distinct r.rental_id) DESC) AS ctrank
+                         row_number() OVER (PARTITION BY ci.customer_id ORDER BY count(distinct r.rental_id) DESC, ct.name ASC) AS ctrank
                   FROM customer_insights ci
                            INNER JOIN rental r ON ci.customer_id = r.customer_id
+                           INNER JOIN payment p ON r.rental_id = p.rental_id    --Consider paid rentals
                            INNER JOIN inventory i ON r.inventory_id = i.inventory_id
                            INNER JOIN film_category fc ON i.film_id = fc.film_id
                            INNER JOIN category ct ON fc.category_id = ct.category_id
+                  WHERE r.rental_date >= '2005-01-01' AND r.rental_date <= '2005-12-31' --Considering 2005 rentals data
                   group by ci.customer_id, ct.name) t1
-            WHERE t1.ctrank =1)
-SELECT * FROM fav_cat LIMIT 10;
-
-
+            WHERE t1.ctrank =1),
+--CTE for primary store customer uses.
+primary_store AS (
+   SELECT t2.customer_id, t2.staff_id, t2.store_id
+   FROM (SELECT ci.customer_id,
+                st.staff_id,
+                st.store_id,
+                COUNT(distinct r.rental_id),
+                row_number() OVER (PARTITION BY ci.customer_id order by COUNT(DISTINCT r.rental_id) DESC, st.store_id ASC) favrank
+         FROM customer_insights ci
+                  INNER JOIN rental r ON ci.customer_id = r.customer_id
+                  INNER JOIN staff st ON r.staff_id = st.staff_id
+         GROUP BY ci.customer_id, st.staff_id, st.store_id) t2
+   WHERE t2.favrank = 1
+)
+SELECT ci.customer_id, concat(C.first_name, ' ', c.last_name), c.email, ci.active_status, ci.risk_status, ci.last_rental_date, ci.days_since_last_rental, ci.life_time_payments,
+       life_time_value_percent_rank*100 AS spent_rank, ci.rental_frequency,
+       ci.total_rentals, fc.name, ps.staff_id, ps.store_id
+FROM customer_insights ci
+JOIN customer c ON ci.customer_id = c.customer_id
+JOIN fav_cat fc ON ci.customer_id = fc.customer_id
+JOIN primary_store ps ON ci.customer_id = ps.customer_id
+ORDER BY spent_rank ASC;
