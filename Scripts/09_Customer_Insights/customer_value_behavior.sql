f1127d0ed56c755c8a97f556863df9207ff94fa4
Hafeezuddin <hafeezskhbablu@gmail.com>
2026-01-03 21:38:38 -0500

    Metric: Customer fav category added based on Number of times he/she rents film from each category

--- Scripts/09_Customer_Insights/customer_value_behavior.sql
+++ Scripts/09_Customer_Insights/customer_value_behavior.sql
@@ -19,20 +19,42 @@ Include both active and inactive customers
 Rank customers by lifetime value percentile
 Flag customers who haven't rented in > 150 days as "at-risk" **/
 
-SELECT c.customer_id, c.first_name, c.email,
-       EXTRACT(MONTH FROM age(MAX(r.rental_date),MIN(r.rental_date))) + 1 AS active_months,
-       SUM(p.amount) AS life_time_payments, to_char(DATE_TRUNC('day', MAX(r.rental_date)), 'YYYY-MM-DD') AS last_rental_date,
-       EXTRACT(DAYS FROM '01-01-2006' - MAX(r.rental_date)) AS days_since_last_rental,
-       COUNT(distinct r.rental_id) AS total_rentals, --Paid rentals
-       ROUND(COUNT(distinct  r.rental_id)::numeric/(EXTRACT(MONTH FROM age(MAX(r.rental_date),MIN(r.rental_date))) + 1),2) AS rental_frequency,
-       CASE
-           WHEN '2006-01-01' - MAX(r.rental_date::date) < 150
-                THEN 'active'
-            ELSE
-                'Inactive'
-       END AS active_status
-FROM customer c
-INNER JOIN rental r ON c.customer_id = r.customer_id
-INNER JOIN payment p ON r.rental_id = p.rental_id
-WHERE r.rental_date >= '2005-01-01' AND r.rental_date <= '2005-12-31'
-GROUP BY c.customer_id, c.first_name, c.email;
+--CTE for customer demographic and total metrics
+WITH customer_insights AS (SELECT c.customer_id,
+                                  EXTRACT(MONTH FROM age(MAX(r.rental_date), MIN(r.rental_date))) + 1             AS active_months,
+                                  SUM(p.amount)                                                                   AS life_time_payments,
+                                  to_char(DATE_TRUNC('day', MAX(r.rental_date)), 'YYYY-MM-DD')                    AS last_rental_date,
+                                  EXTRACT(DAYS FROM '01-01-2006' - MAX(r.rental_date))                            AS days_since_last_rental,
+                                  COUNT(distinct r.rental_id)                                                     AS total_rentals, --Paid rentals
+                                  ROUND(COUNT(distinct r.rental_id)::numeric /
+                                        (EXTRACT(MONTH FROM age(MAX(r.rental_date), MIN(r.rental_date))) + 1),
+                                        2)                                                                        AS rental_frequency,
+                                  CASE
+                                      WHEN '2006-01-01' - MAX(r.rental_date::date) < 150
+                                          THEN 'active'
+                                      ELSE
+                                          'Inactive'
+                                      END                                                                         AS active_status
+                           FROM customer c
+                                    INNER JOIN rental r ON c.customer_id = r.customer_id
+                                    INNER JOIN payment p ON r.rental_id = p.rental_id
+                           WHERE r.rental_date >= '2005-01-01'
+                             AND r.rental_date <= '2005-12-31'
+                           GROUP BY c.customer_id
+                           ),
+--CTE for each customers fav category
+fav_cat AS (SELECT t1.customer_id, t1.name
+            FROM (SELECT ci.customer_id,
+                         ct.name,
+                         COUNT(DISTINCT r.rental_id),
+                         RANK() OVER (PARTITION BY ci.customer_id ORDER BY count(distinct r.rental_id) DESC) AS ctrank
+                  FROM customer_insights ci
+                           INNER JOIN rental r ON ci.customer_id = r.customer_id
+                           INNER JOIN inventory i ON r.inventory_id = i.inventory_id
+                           INNER JOIN film_category fc ON i.film_id = fc.film_id
+                           INNER JOIN category ct ON fc.category_id = ct.category_id
+                  group by ci.customer_id, ct.name) t1
+            WHERE t1.ctrank =1)
+SELECT * FROM fav_cat LIMIT 10;
+
+
