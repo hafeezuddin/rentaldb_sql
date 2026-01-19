bfc170a0f774f52d366f449fd8a15564f7e9f489
Hafeezuddin <hafeezskhbablu@gmail.com>
2026-01-19 12:44:38 -0500

    ADD:Script list customers who rented most expensive films and Ordered in desc based on number of times they rented such expensive films

---
+++ Scripts/03_Ranking_Analysis/07_pr_customers_rented_most_expensive_films.sql
@@ -0,0 +1,16 @@
+/* Customers who rented the most expensive movie */
+--CTE to find price of most expensive film
+WITH expensive_film AS (
+    SELECT f.film_id
+    FROM film f
+    WHERE f.rental_rate = (SELECT MAX(f.rental_rate) FROM film f)
+)
+SELECT c.customer_id, CONCAT(c.first_name,' ', c.last_name) AS full_name,
+       c.email,
+       COUNT(*) AS no_of_times_rented
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN expensive_film ef ON i.film_id = ef.film_id
+GROUP BY c.customer_id
+ORDER BY no_of_times_rented DESC;
