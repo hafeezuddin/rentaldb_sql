6bcd50e160986e30b7835c5dfb2f46cd4a3b0820
Hafeezuddin <hafeezskhbablu@gmail.com>
2026-01-30 21:00:43 -0500

    ADD:top five months based on rental activity and their share in total over all revenue

---
+++ Scripts/03_Ranking_Analysis/17_top_five_rev_generating_months.sql
@@ -0,0 +1,22 @@
+/*Find the top 5 months (across all years) with the highest rental activity.
+For each of these months, show:
+    Year and Month (e.g., 2021-05),
+    Total number of rentals,
+    Total revenue collected (from payment.amount)
+The percentage share of revenue compared to the overall revenue.*/
+
+WITH base_rental_data AS (SELECT date_trunc('Month', r.rental_date) AS truncated_date,
+                                 SUM(p.amount)                      AS total_revenue,
+                                 COUNT(DISTINCT r.rental_id)        AS total_rentals,
+                                 SUM(SUM(p.amount)) OVER ()         AS total_revenue_overall
+                          FROM rental r
+                                   JOIN payment p ON r.rental_id = p.rental_id
+                          GROUP BY 1)
+
+SELECT to_char(brd.truncated_date, 'YYYY-MON')                                               AS year_month_head,
+       brd.total_revenue,
+       brd.total_rentals,
+       ROUND(coalesce(brd.total_revenue, 0) / NULLIF(brd.total_revenue_overall, 0) * 100, 2) AS revenue_share
+FROM base_rental_data brd
+ORDER BY revenue_share DESC
+LIMIT 5;
